da82cc04441cf977ee62de0474b77ee53f186406
prathamesh8805 <bhongaleprathamesh@gmail.com>
2022-05-08 16:07:32 +0530

    Commit 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,33 @@ import { BodyComponent } from './body/body.component';
 import { TodoComponent } from './body/todo/todo.component';
 import { MatIconModule } from '@angular/material/icon';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { InprogessComponent } from './body/inprogess/inprogess.component';
+import { CompletedComponent } from './body/completed/completed.component';
+import { ModalComponent } from './modal/modal.component';
+import { HeaderComponent } from './header/header.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgModel } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent, BodyComponent, TodoComponent],
-  imports: [BrowserModule, AppRoutingModule, MatIconModule, NoopAnimationsModule],
+  declarations: [
+    AppComponent,
+    BodyComponent,
+    TodoComponent,
+    InprogessComponent,
+    CompletedComponent,
+    ModalComponent,
+    HeaderComponent,
+  ],
+  imports: [
+    BrowserModule,
+    NgModel,
+
+    AppRoutingModule,
+    MatIconModule,
+    NoopAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
