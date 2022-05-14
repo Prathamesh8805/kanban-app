9d5953c0d9fd4c73b0a8d19b0cd7a60c2ea7d6a4
prathamesh8805 <bhongaleprathamesh@gmail.com>
2022-05-14 13:03:45 +0530

    Commit 4

--- src/app/modal/modal.component.ts
+++ src/app/modal/modal.component.ts
@@ -16,6 +16,7 @@ export class ModalComponent implements OnInit {
 
   constructor(public postService: PostService) {}
   addPost(form: NgForm) {
+    console.log('received the post', form);
     this.postService.addPostService(
       form.value.title,
       form.value.content,
